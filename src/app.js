327c695fd0b2d44f6aad56bd579395a4970bb119
joaquim <joaquim.claudio2003@gmail.com>
2024-08-28 04:49:17 +0100

    Created a route for connection tests.

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
+import pingRouter from './routes/ping.js';
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(cookieParser());
 app.use(express.static('public'));
 
 app.use('/', indexRouter);
+app.use('/ping', pingRouter);
 
 export default app;
