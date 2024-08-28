bbe2d8dfc9fb07bb0485f8449088109e61b76a6b
joaquim <joaquim.claudio2003@gmail.com>
2024-08-28 01:10:30 +0100

    Web server configuration.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import indexRouter from './routes/index.js';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+export default app;
